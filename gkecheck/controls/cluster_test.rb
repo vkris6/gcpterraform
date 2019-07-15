c86ecf5899877fa5a4f344742dfa2224dc5cc22e
vkris6 <vkrishnamurthy@sapient.com>
2019-07-15 23:56:34 +0100

    testcases

---
+++ gkecheck/controls/cluster_test.rb
@@ -0,0 +1,43 @@
+title 'Check GKE Cluster'
+
+PROJECT_NAME = attribute('project_name', description: 'gcp project name')
+CLUSTER_NAME = attribute('cluster_name', description: 'gcp cluster name')
+CLUSTER_ZONE = attribute('cluster_zone', description: 'gcp cluster zone')
+
+control 'gcp-1' do
+  impact 0.7
+  title 'Check GKE Cluster'
+
+  describe google_container_cluster(project: PROJECT_NAME, zone: CLUSTER_ZONE, name: CLUSTER_NAME) do
+    it { should exist }
+    its('name') { should eq CLUSTER_NAME }
+    its('status') { should eq 'RUNNING' }
+    its('zone') { should match CLUSTER_ZONE }
+    its('master_auth.username') { should eq 'admin' }
+    its('network') { should eq 'default' }
+    its('subnetwork') { should eq 'default' }
+    its('initial_node_count') { should eq 3 }
+    its('node_config.disk_size_gb'){should eq 100}
+    its('node_config.image_type'){should eq "COS"}
+    its('node_config.machine_type'){should eq "n1-standard-1"}
+    its('node_ipv4_cidr_size'){should eq 24}
+    its('node_pools.count'){should eq 1}
+  end
+end
+
+
+google_container_node_pools(project: PROJECT_NAME, zone: CLUSTER_ZONE, cluster_name: CLUSTER_NAME).node_pool_names.each do |node_pool_name|
+  describe google_container_node_pool(project: PROJECT_NAME, zone: CLUSTER_ZONE, cluster_name: CLUSTER_NAME, nodepool_name: node_pool_name) do
+    it { should exist }
+    its('status') { should eq 'RUNNING' }
+  end
+end
+
+google_compute_networks(project: PROJECT_NAME).network_names.each do |network_name|
+  describe google_compute_network(project: PROJECT_NAME,  name: network_name) do
+    its ('subnetworks.count') { should be < 10 }
+    its ('creation_timestamp_date') { should be > Time.now - 365*60*60*24*10 }
+    its ('routing_config.routing_mode') { should eq "REGIONAL" }
+    its ('auto_create_subnetworks'){ should be false }
+  end
+end
